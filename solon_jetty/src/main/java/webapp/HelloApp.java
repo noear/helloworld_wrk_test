c8a66f8c01bfa7723d5cf6f9a9f30b7faef8a7a5
noear <noear@live.cn>
2020-10-05 13:46:36 +0800

    demo

---
+++ solon_jetty/src/main/java/webapp/HelloApp.java
@@ -0,0 +1,17 @@
+package webapp;
+
+import org.noear.solon.XApp;
+import org.noear.solon.annotation.XController;
+import org.noear.solon.annotation.XMapping;
+
+@XController
+public class HelloApp {
+    public static void main(String[] args) {
+        XApp.start(HelloApp.class, args);
+    }
+
+    @XMapping("/")
+    public String hello(String name){
+        return "hello world!";
+    }
+}
