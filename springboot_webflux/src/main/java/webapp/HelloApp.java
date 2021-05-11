e9d95f92342d55d6c58179704751f157debd2e44
noear <noear@live.cn>
2021-05-11 12:39:26 +0800

    test

---
+++ springboot_webflux/src/main/java/webapp/HelloApp.java
@@ -0,0 +1,20 @@
+package webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@SpringBootApplication
+public class HelloApp {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApp.class, args);
+    }
+
+    @RequestMapping("/")
+    public Mono<String> hello(String name) {
+        return Mono.just("hello world: " + name);
+    }
+}
