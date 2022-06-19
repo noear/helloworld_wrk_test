e78e67b11e5d2788e9215d7fde920bd7072baf1a
noear <noear@live.cn>
2022-06-19 22:53:02 +0800

    test

---
+++ solon_jdkhttp/src/main/java/webapp/HelloApp.java
@@ -0,0 +1,18 @@
+package webapp;
+
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+
+@Controller
+public class HelloApp {
+    public static void main(String[] args) {
+        Solon.start(HelloApp.class, args);
+    }
+
+    @Mapping("/")
+    public String hello(String name) {
+        return "hello world: " + name;
+    }
+}
