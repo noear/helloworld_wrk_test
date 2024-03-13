141c5bae1d7b367a3e94d7ceb4eeeb82ab7f7925
noear <noear@live.cn>
2024-03-13 11:32:52 +0800

    test

--- _socketd_server/src/main/java/sdapp/ServerApp.java
+++ _socketd_server/src/main/java/sdapp/ServerApp.java
@@ -1,21 +1,9 @@
 package sdapp;
 
-import org.noear.socketd.SocketD;
-import org.noear.socketd.transport.core.entity.StringEntity;
-import org.noear.socketd.transport.core.listener.EventListener;
+import org.noear.solon.Solon;
 
 public class ServerApp {
     public static void main(String[] args) throws Exception {
-        //启动服务端
-        SocketD.createServer("sd:tcp")
-                .config(c -> c.port(18602))
-                .listen(new EventListener()
-                        .doOn("hello", (s, m) -> { //收到"/demo"事件的消息时
-                            if (m.isRequest() || m.isSubscribe()) {
-                                //答复
-                                s.replyEnd(m, new StringEntity("And me too."));
-                            }
-                        }))
-                .start();
+        Solon.start(ServerApp.class, args);
     }
 }
