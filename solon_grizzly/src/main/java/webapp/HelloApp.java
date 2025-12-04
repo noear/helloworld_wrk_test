43219bea68bee5ae10dba1f52e7245758f0e3c89
noear <noear@live.cn>
2025-12-04 19:25:47 +0800

    test

---
+++ solon_grizzly/src/main/java/webapp/HelloApp.java
@@ -0,0 +1,43 @@
+package webapp;
+
+
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Get;
+import org.noear.solon.annotation.Mapping;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Controller
+public class HelloApp {
+    public static void main(String[] args) {
+        Solon.start(HelloApp.class, args);
+    }
+
+    @Get
+    @Mapping("/")
+    public String hello(String name) {
+        return "hello world: " + name;
+    }
+
+    @Get
+    @Mapping("/hello2")
+    public String hello2(String name) throws Exception{
+        Thread.sleep(10);
+        return "hello world: " + name;
+    }
+
+    @Get
+    @Mapping("/rx")
+    public Mono<String> rx(String name) {
+        return Mono.just("hello world: " + name);
+    }
+
+    @Get
+    @Mapping("/rx2")
+    public Mono<String> rx2(String name) {
+        return Mono.delay(Duration.ofMillis(10))
+                .then(Mono.just("hello world: " + name));
+    }
+}
